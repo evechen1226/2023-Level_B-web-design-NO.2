72b75f8c95b2d206466459d75f97013db17b7ab7
evechen1226 <eveliving2021@gmail.com>
2023-12-29 11:52:59 +0800

    feat:front/reg.php-完成畫面建置

--- front/reg.php
+++ front/reg.php
@@ -1,1 +1,26 @@
-
+<fieldset class="cent" style="width: 50%;">
+    <legend>會員註冊</legend>
+    <span style="color:red;">*請設定您要註駃的帳號及密（最長12個字元）</span>
+    <table>
+        <tr>
+            <td class="clo">Step1:登入帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td class="clo">Step2:登入密碼</td>
+            <td><input type="password" name="pwd" id="pwd"></td>
+        </tr>
+        <tr>
+            <td class="clo">Step3:再次確認密碼</td>
+            <td><input type="password" name="pwd2" id="pwd2"></td>
+        </tr>
+        <tr>
+            <td class="clo">Step4:信箱（忘記密碼時使用）</td>
+            <td><input type="text" name="email" id="email"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="註冊"><input type="reset" value="清除"></td>
+            <td></td>
+        </tr>
+    </table>
+</fieldset>
