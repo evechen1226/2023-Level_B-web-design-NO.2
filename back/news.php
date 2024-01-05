e1f776dda255d6ab668d5d7dcee68a9e9d2f7921
evechen1226 <eveliving2021@gmail.com>
2024-01-05 13:27:01 +0800

    feat:api/edit_news.php、back/news.php--增加api/edit_new.php頁面，加入顯示及刪除功能，news.php增加 input:hidden的 id 列

--- back/news.php
+++ back/news.php
@@ -1,5 +1,5 @@
-<form action="./edit_news.php" method="post" class="ct">
-        <table class="style:text">
+<form action="./api/edit_news.php" method="post" >
+        <table style="width:75%;text-align:center;">
             <tr>
                 <td>編號</td>
                 <td>顯示</td>
@@ -10,25 +10,18 @@
                 foreach ($rows as $row) {
                 ?>
             </tr>
+            
             <td><?= $row['title']; ?></td>
-            <td><input type="checkbox" name="del[]" value="<?=$row['id']; ?>" <?=($row['sh'] == 1)?'checked': '';?>></td>
+            <td><input type="checkbox" name="sh[]" value="<?=$row['id']; ?>" <?=($row['sh'] == 1)?'checked': '';?>></td>
             <td><input type="checkbox" name="del[]" value="<?=$row['id']; ?>"></td>
+            <input type="hidden" name="id[]" value="<?=$row['id']?>">
             <tr>
         <?php
                     
                 }
         ?>
             </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+
         </table>
         <button type="submit">確定修改</button>
         
