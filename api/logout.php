29e807f5e53d4ce37440a184f27fcab81e5eface
evechen1226 <eveliving2021@gmail.com>
2024-01-05 11:30:53 +0800

    feat:back/admin.php--新增帳號管理功能及頁面

--- api/logout.php
+++ api/logout.php
@@ -2,3 +2,5 @@
 session_start();
 unset($_SESSION['user']);
 header("location:../index.php");
+
+?>
--- back/admin.php
+++ back/admin.php
@@ -1,41 +1,42 @@
-<fieldset class="cent" style="width: 70%;">
-    <form action="" method="post"></form>
-    <?php
-    $rows = $User->all();
-    ?>
+<fieldset class="cent" style="width: 90%;">
+<legend>帳號管理</legend>    
+<form action="./edit_user.php" method="post">
+        <table>
+            <tr>
+                <td>帳號</td>
+                <td>密碼</td>
+                <td>刪除</td>
 
-    <table>
-        <tr>
-            <td>帳踐</td>
-            <td>密碼</td>
-            <td>刪除</td>
-        </tr>
+                <?php
+                $rows = $User->all();
+                foreach ($rows as $row) {
+                    if ($row['acc'] != 'admin') {
+                ?>
+            </tr>
+            <td><?= $row['acc']; ?></td>
+            <td><?= $row['pw']; ?></td>
+            <td><input type="checkbox" name="del[]" value="<?= $row['id']; ?>"></td>
+            <tr>
         <?php
-        foreach ($rows as $rows) {
+                    }
+                }
         ?>
-         <td><?=$row['acc'];?></td>
-            <td><?=$row['pw'];?></td>
-            <td><input type="checbox"name="del[]"value="<?=$row['id'];?>"></td>
-        <?php
-        }
-        ?>
-        <tr>
-
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>
-    <button type="submit">確定刪除</button>
-    <button type="rest">清空選取</button>
-    <legend>帳號管理</legend>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+        <button type="submit">確定刪除</button>
+        <button type="rest">清空選取</button>
+    </form>
+    
     <h2>新增會員</h2>
     <span style="color:red;">*請設定您要註冊的帳號及密碼（最長12個字元）</span>
     <table>
