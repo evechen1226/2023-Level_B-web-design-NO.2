7c0e35e1ca4f3d8d20b6c3a59ce5b529ec6cc547
evechen1226 <eveliving2021@gmail.com>
2024-01-05 16:05:46 +0800

    feat:api/get_list.php、front/po.php--增加api/get_list.php頁面，使用ajax來載入分類網誌的標題

--- front/po.php
+++ front/po.php
@@ -9,7 +9,8 @@
         display: inline-block;
         vertical-align: top;
     }
-    .news-list{
+
+    .news-list {
         width: 600px;
     }
 </style>
@@ -17,20 +18,32 @@
 
 <fieldset class="types">
     <legend>分類網誌</legend>
-    <a class="type-item">健康新知</a>
-    <a class="type-item">菸害防治</a>
-    <a class="type-item">癌症防治</a>
-    <a class="type-item">慢性病防治</a>
+    <a class="type-item" data-id="1">健康新知</a>
+    <a class="type-item" data-id="2">菸害防治</a>
+    <a class="type-item" data-id="3">癌症防治</a>
+    <a class="type-item" data-id="4">慢性病防治</a>
 </fieldset>
 <fieldset class="news-list">
     <legend>文章列表</legend>
-<div class="list-items"></div>
-<div class="artcle"></div>
+    <div class="list-items">
+
+    </div>
+    <div class="article"></div>
 
 </fieldset>
 <script>
+    getList(1);
+
     $('.type-item').on('click', function() {
         $('.type').text($(this).text())
+        let type = $(this).data('id') //date() 拿到資料，有可能是 數字 或 字串
+        getList(type)
     })
 
+    function getList(type) {
+        // {type:type} , {name:type} , {name:type,...,...,}, 如果key與valuet相同，可以只寫一次
+        $.get("./api/get_list.php", {type}, (list) => {
+            $('.list-items').html(list)
+        })
+    }
 </script>
