5928bdc4d8485865b044fa40fe61833f8d25a497
evechen1226 <eveliving2021@gmail.com>
2024-01-08 16:18:43 +0800

    feat:api/vote.php、front/result.php、vote.php--修正打字錯誤導致參數Undefined

--- api/vote.php
+++ api/vote.php
@@ -1,1 +1,11 @@
-<?
+<?php include_once "db.php";
+
+$opt=$Que->find($_POST['opt']);
+$opt['vote']++;
+$Que->save($opt);
+$sub=$Que->find($opt['subject_id']);
+$sub['vote']++;
+$Que->save($sub);
+
+// 也可以寫 $opt['subject_id']
+to("../index.php?do=result&id={$sub['id']}");
--- front/vote.php
+++ front/vote.php
@@ -3,19 +3,21 @@ $que = $Que->find($_GET['id'])
 ?>
 
 <fieldset>
-    <legend>目前位置：首頁 > 問卷調查 > <?= $que['text'] ?></legend>
-
+    <legend>目前位置：首頁 > 問卷調查 > <?= $que['text']; ?></legend>
+    <h3><?= $que['text']; ?></h3>
+    <form action="./api/vote.php" method="post">
     <?php
-    $opt = $Que->all(['subject_id' => $_GET['id']]);
+    $opts = $Que->all(['subject_id' => $_GET['id']]);
     foreach ($opts as $opt) {
         echo "<div>";
-        echo "<input type='radio' name='' value='{$opt['id']}'>";
+        echo "<input type='radio' name='opt' value='{$opt['id']}'>";
         echo $opt['text'];
         echo "</div>";
     }
 
     ?>
     <div class="ct">
-        <input type="button" value="submit">
+        <input type="submit" value="我要投票">
     </div>
+    </form>
 </fieldset>
